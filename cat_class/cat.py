3a6b04dc8c8cef86a3a29a54e89c0b834dfb938f
Brody Currie <brodycurrie@gmail.com>
2019-05-10 13:50:50 -0400

    Completed steps 1 - 4

--- cat_class/cat.py
+++ cat_class/cat.py
@@ -1,1 +1,17 @@
-dfkhwef
+class Cat:
+    """ This is a class defining a cat """ 
+
+    def __init__(self, name, preferred_food, meal_time):
+        self.name = name
+        self.preferred_food = preferred_food
+        self.meal_time = meal_time
+
+    def __str__(self):
+        return f"{self.name} eats their {self.preferred_food} at {self.meal_time}"
+
+
+tibby = Cat('Tibby', 'Chicken Pate', 9)
+frank = Cat('Frank', 'Dog Kibble', 20)
+
+print(tibby)
+print(frank)
