524b70859f772761649c25dc9c458703a7f90040
Brody Currie <brodycurrie@gmail.com>
2019-05-10 12:55:30 -0400

    Compeleted reinforcement exercise

---
+++ Reinforcement.py
@@ -0,0 +1,22 @@
+venues = [
+{ 'address': "123 Main Street", 'city': "Toronto", 'wheelchair_accessible': True, 'capacity': 100 },
+{ 'address': "567 Centre Street", 'city': "Toronto", 'wheelchair_accessible': False, 'capacity': 400 },
+{ 'address': "9B Ontario Street", 'city': "Montreal", 'wheelchair_accessible': True, 'capacity': 1000 },
+{ 'address': "56 Road Avenue", 'city': "Ottawa", 'wheelchair_accessible': True, 'capacity': 650 },
+{ 'address': "938 Avenue Way East", 'city': "Toronto", 'wheelchair_accessible': False, 'capacity': 90 },
+{ 'address': "34 Main Street West", 'city': "London", 'wheelchair_accessible': False, 'capacity': 300 },
+{ 'address': "44 Quebec Road", 'city': "Toronto", 'wheelchair_accessible': True, 'capacity': 200 },
+{ 'address': "10 Spruce Avenue Ouest", 'city': "Montreal", 'wheelchair_accessible': False, 'capacity': 525 }
+
+]
+
+new_venue_list = []
+
+def find_new_venues(list):
+    for dict in list:
+        if dict['wheelchair_accessible'] == True and dict['capacity'] >= 150:
+            new_venue_list.append(dict)
+
+find_new_venues(venues)
+
+print(new_venue_list)
